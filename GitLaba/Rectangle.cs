8cc4a9c3ca72c81a71d618c243259359358de39b
Руслан Вольвач <volvachruslann@mail.ru>
2026-09-09 21:39:01 +0300

    Добавил класс четырёхугольника

---
+++ GitLaba/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitLaba
+{
+    public class Rectangle
+    {
+        public Point2D P1 {  get; private set; }
+        public Point2D P2 { get; private set; }
+        public Point2D P3 { get; private set; }
+        public Point2D P4 { get; private set; }
+        public Rectangle(Point2D p1, Point2D p2, Point2D p3, Point2D p4)
+        {
+            P1 = p1;
+            P2 = p2;
+            P3 = p3;
+            P4 = p4;
+        }
+    }
+}
