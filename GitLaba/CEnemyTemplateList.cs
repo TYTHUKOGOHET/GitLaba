1e962676cff1ea699ca7b5c3f2dc80468ebfb29d
Dmitriy Bystrov <bystrovdima47@gmail.com>
2026-09-16 19:05:28 +0700

    добавление класса лист врагов

---
+++ GitLaba/CEnemyTemplateList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitLaba
+{
+    public class CEnemyTemplateList
+    {
+        //Список противников из класса CEnemyTemplate
+        List<CEnemyTemplate> enemies;
+        public CEnemyTemplateList()
+        {
+            enemies = new List<CEnemyTemplate>();
+        }
+    }
+
+}
